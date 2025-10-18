18fac4a64ddf5255af5acab74169004d26374811
Antonio Giroz <antonioluisgil@gmail.com>
2025-10-18 17:02:25 +0200

    Initial Commit

---
+++ HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by Antonio Giroz on 18/10/25.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
